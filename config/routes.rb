cb4145f51c0fa4fc897772995e358ed7859fc60b
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-08-31 05:57:07 +0000

    Improve the README file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'courses/index' => 'course#index'
-  get 'courses/new' => 'course#new'
-  get 'courses/show' => 'course#show'
-  get 'courses/edit' => 'course#edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
