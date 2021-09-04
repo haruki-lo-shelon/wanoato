6fed6029232462f1b57313bec0e082f0fca48ed4
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-09-04 15:14:02 +0000

    Improve the README file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :courses, dependent: :destroy
+  
+  has_many :likes, dependent: :destroy
+  has_many :liked_courses, through: :likes, source: :course
+  
+  def already_liked?(course)
+    self.likes.exists?(course_id: course.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_124701) do
+ActiveRecord::Schema.define(version: 2021_09_04_111508) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2021_08_30_124701) do
     t.string "area"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_likes_on_course_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_08_30_124701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "courses"
+  add_foreign_key "likes", "users"
 end
