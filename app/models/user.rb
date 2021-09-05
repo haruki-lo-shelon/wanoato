2160b0a4fc56d223cf7c1695af698448121257a3
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-09-05 01:00:56 +0000

    Improve the README file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :courses
+  has_many :courses, dependent: :destroy
   
   has_many :likes, dependent: :destroy
   has_many :liked_courses, through: :likes, source: :course
