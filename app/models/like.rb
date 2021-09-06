68175d0f0df518b00dfb966e56783f0a8980f9be
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-09-06 11:24:17 +0000

    Improve the README file

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  # belongs_to :course
-  # belongs_to :user
+  belongs_to :course
+  belongs_to :user
   
-  # validates_uniqueness_of :course_id, scope: :user_id
+  validates_uniqueness_of :course_id, scope: :user_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 以下の二行をdevise_for :usersの下側に追記
-  resources :courses 
-  # do
-  #   resources :likes, only: [:create, :destroy]
-  # end
+  resources :courses do
+    resources :likes, only: [:create, :destroy]
+  end
   root 'courses#index'
 end
