8e994d1af9510126446e80155bbdcf1f72ef4910
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-09-11 06:56:06 +0000

    Improve the README file

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,10 +5,10 @@ class RelationshipsController < ApplicationController
     following = current_user.follow(@user)
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to users_path
+      redirect_to user_path
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to users_path
+      redirect_to user_path
     end
   end
 
@@ -16,10 +16,10 @@ class RelationshipsController < ApplicationController
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to users_path
+      redirect_to user_path
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to users_path
+      redirect_to user_path
     end
   end
 
