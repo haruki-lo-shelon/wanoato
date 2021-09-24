830bc8e441be5675b25680bd2af1bf62e55261c3
haruki-lo-shelon <haruki.lo.shelon@gmail.com>
2021-09-24 04:11:34 +0000

    Improve the README file

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :user
-  mount_uploader :image, :image2, :image3, :image4, :image5, ImageUploader
+  mount_uploader :image, ImageUploader
+  mount_uploader :image2, ImageUploader
+  mount_uploader :image3, ImageUploader
+  mount_uploader :image4, ImageUploader
+  mount_uploader :image5, ImageUploader
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
